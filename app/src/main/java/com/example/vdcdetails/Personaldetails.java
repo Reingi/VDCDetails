f7af9846a5c1b13ebbe344e5eb3e74c4e56e556b
Lakpa Ringi Sherpa <devilsground@gmail.com>
2019-10-01 14:49:37 +0545

    Update Personaldetails.java

--- app/src/main/java/com/example/vdcdetails/Personaldetails.java
+++ app/src/main/java/com/example/vdcdetails/Personaldetails.java
@@ -6,8 +6,15 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioGroup;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -18,11 +25,13 @@ import androidx.fragment.app.Fragment;
  * Use the {@link Personaldetails#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Personaldetails extends Fragment {
+public class Personaldetails extends Fragment implements View.OnClickListener{
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    RadioGroup radiogrp;
+    TextInputLayout spouseprofession,abroadfrm;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -55,6 +64,7 @@ public class Personaldetails extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -64,17 +74,60 @@ public class Personaldetails extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_personaldetails, container, false);
+
+        radiogrp = (RadioGroup) view.findViewById(R.id.radioGroup);
+        spouseprofession =(TextInputLayout) view.findViewById(R.id.spouse_profession);
+        abroadfrm = (TextInputLayout) view.findViewById(R.id.abroadfrom);
+
+        radiogrp.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                if(i == R.id.yes){
+                    spouseprofession.setVisibility(View.VISIBLE);
+                    abroadfrm.setVisibility(View.VISIBLE);
+                }else{
+                    spouseprofession.setVisibility(View.GONE);
+                    abroadfrm.setVisibility(View.GONE);
+                }
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personaldetails, container, false);
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+    private Button nextBT;
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        nextBT = view.findViewById(R.id.nextBT);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        nextBT.setOnClickListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        nextBT.setOnClickListener(null);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()) {
+            case R.id.nextBT:
+                if (mListener != null)
+                    mListener.onNextPressed(this);
+                break;
         }
-    }
 
+    }
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -90,6 +143,7 @@ public class Personaldetails extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+        nextBT = null;
     }
 
     /**
@@ -103,7 +157,7 @@ public class Personaldetails extends Fragment {
      * >Communicating with Other Fragments</a> for more information.
      */
     public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+        //void onFragmentInteraction(Uri uri);
+        void onNextPressed(Fragment fragment);
     }
 }
